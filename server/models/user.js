e6e50ba545f3bbbb2aa80ae47641332d6bef5f47
Paterson <djemipaterson@yahoo.fr>
2017-10-19 14:58:25 +0200

    Added route users/login

--- server/models/user.js
+++ server/models/user.js
@@ -98,7 +98,35 @@ mySchema.pre('save', function (next) {
 
         next();
     }
-})
+});
+
+mySchema.statics.findByCredentials =  function (email, password) {
+    var User = this;
+
+    return User.findOne({
+            email
+        })
+        .then(user => {
+            if(!user) {
+                return Promise.reject();
+            }
+
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                   if (res) {
+                       resolve(user);
+                   } else {
+                       reject();
+                   }
+                });
+            });
+
+    })
+
+
+
+
+};
 
 var User = mongoose.model("User", mySchema);
 
